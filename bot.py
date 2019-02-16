fbddfa95faf5b612df7965201123d389b8261a96
Kelly Ervin <kellyme@vt.edu>
2019-02-16 17:05:02 -0500

    Combine RSSParser.py and bot.py into bot.py and post the most recent Reductress article

--- bot.py
+++ bot.py
@@ -2,17 +2,20 @@ import praw
 import pdb
 import re
 import os
+import feedparser
+
+
+# Create a feedparser instance for the Reductress RSS feed
+feed = feedparser.parse("http://reductress.com/rss")
+
+# Pull down current entries
+num_entries = len(feed.entries)
+title = feed.entries[0].title
+url = feed.entries[0].link
 
 # Create the Reddit instance and log in using praw credentials
 # located in local praw.ini file
 reddit = praw.Reddit('ReductressBot')
 
-# Pull the hottest 10 entries from r/aww and print the titles
-# subreddit = reddit.subreddit('aww')
-# for submission in subreddit.hot(limit=10):
-#     print(submission.title)
-
 subreddit = reddit.subreddit('reductress')
-title = 'Test Post 2 - Please Ignore'
-url = 'https://imgur.com/gallery/87dU6iV'
 subreddit.submit(title, url=url)
